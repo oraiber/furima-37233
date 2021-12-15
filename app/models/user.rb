badd9bd0568470a0914a7a0687d25176d7e5ef16
oraiber <ginsan985@gmail.com>
2021-12-15 20:17:21 +0900

    修正_3

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+  # has_many :items
+  # has_many :orders
 
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で設定してください'}
   validates :nickname,          presence: true
