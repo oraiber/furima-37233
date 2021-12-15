1ab926c3de304f88dd566592887855598ee1489e
oraiber <ginsan985@gmail.com>
2021-12-15 15:31:01 +0900

    ユーザーin/out

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birth_day, presence: true
+end
