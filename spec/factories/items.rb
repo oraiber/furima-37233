864870f0a4fce74cb97439916493998e245536dd
oraiber <ginsan985@gmail.com>
2021-12-17 12:22:55 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name     {'名前'}
+    text          {Faker::Lorem.sentence}
+    item_condition_id  {2}
+    category_id        {2}
+    delivery_charge_id {2}
+    area_id            {2}
+    days_id            {2}
+    price           {2000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/image/sample.jpeg'), filename: 'sample.jpeg')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品情報の入力' do
+    context '商品情報入力がうまくいく時' do
+      it '全ての値が正しく入力されていれば出品できること' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品情報の入力がうまく行かない時' do
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空だと出品できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'textが空だと出品できない' do
+        @item.text = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'categoryが未選択だと出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'item_conditionが未選択だと出品できない' do
+        @item.item_condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item condition can't be blank")
+      end
+      it 'delivery_chargeが未選択だと出品できない' do
+        @item.delivery_charge_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it 'areaが未選択だと出品できない' do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank")
+      end
+      it 'daysが未選択だと出品できない' do
+        @item.days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days can't be blank")
+      end
+      it 'priceが空だと出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角数字だと出品できない' do
+        @item.price = "２０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+
+  end
 end
