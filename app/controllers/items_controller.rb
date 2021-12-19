1f1a482562a0049ac4d9f7ba0c461ecd02fcb09f
oraiber <ginsan985@gmail.com>
2021-12-19 20:11:51 +0900

    詳細ページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,6 +19,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:item_name, :text, :category_id, :item_condition_id, :delivery_charge_id, :area_id, :days_id, :price, :image).merge(user_id: current_user.id)
