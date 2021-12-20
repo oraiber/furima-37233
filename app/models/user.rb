c3b5099f40735153e042959fd9eeb63d43e7445a
oraiber <ginsan985@gmail.com>
2021-12-20 17:47:13 +0900

    orders作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
   # has_many :orders
 
   validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で設定してください'}
